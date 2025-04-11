e921a6dc5c0f9aa0465c10962603fff113c9cacc
Will Squire <will@willsquire.com>
2025-04-12 00:01:48 +0100

    fixed node to return boolean

--- src/node/node.test.ts
+++ src/node/node.test.ts
@@ -1,9 +1,9 @@
 import { test, expect } from "vitest";
 import type { Node } from "./node";
 
-test("should return blackboard", async () => {
-  const blackboard = {};
-  const node: Node<typeof blackboard> = async (blackboard) => blackboard;
+test("should return a boolean", async () => {
+  const blackboard = { value: true };
+  const node: Node<typeof blackboard> = async (blackboard) => blackboard.value;
 
-  expect(await node({})).toStrictEqual(blackboard);
+  expect(await node(blackboard)).toEqual(true);
 });
